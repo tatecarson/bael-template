ea8704565a1eb475e441e96b21062c753b2db012
tatecarson <tatecarson@gmail.com>
2020-07-24 13:49:14 -0500

    added no-ssr

--- components/BaelGrid.vue
+++ components/BaelGrid.vue
@@ -1,24 +1,30 @@
 <template>
-<div class="xs-text-6 md-text-5">
-    <div v-if="items2[0]" class="r full-height browse" :style="`height:calc(100vh - ${navbarheight}px);margin-top:${navbarheight}px`">
-
+  <div class="xs-text-6 md-text-5">
+    <div
+      v-if="items2[0]"
+      class="r full-height browse"
+      :style="`height:calc(100vh - ${navbarheight}px);margin-top:${navbarheight}px`"
+    >
+    <no-ssr>
       <div v-if="items2[pi]" v-for="(p,pi) in items2" :key="p.pi" class="xs-border xs-p2 bcg-item">
         <div class="item xs-block xs-full-height xs-flex">
-          <nuxt-link class="xs-text-center xs-flex xs-full-height xs-flex-align-center xs-flex-justify-center xs-text-center" :to="p._path">
-            {{p.title}}
-          </nuxt-link>
+          <nuxt-link
+            class="xs-text-center xs-flex xs-full-height xs-flex-align-center xs-flex-justify-center xs-text-center"
+            :to="p._path"
+          >{{p.title}}</nuxt-link>
         </div>
       </div>
-
+      </no-ssr>
     </div>
+    
     <div v-else class="r full-height browse">
-      <div class="xs-p2 c-100 xs-flex xs-flex-align-center xs-flex-justify-center xs-text-center" :style="`height:calc(100vh - ${navbarheight}px);margin-top:${navbarheight}px`">
-
-        
+      <div
+        class="xs-p2 c-100 xs-flex xs-flex-align-center xs-flex-justify-center xs-text-center"
+        :style="`height:calc(100vh - ${navbarheight}px);margin-top:${navbarheight}px`"
+      >
         <div v-if="total < 1 && !busy">No Results.</div>
       </div>
-
-  </div>
+    </div>
   </div>
 </template>
 
@@ -33,7 +39,7 @@ export default {
       items2: [],
       query: 1,
       busy: false,
-      count: 0
+      count: 0,
     };
   },
   methods: {
@@ -53,17 +59,15 @@ export default {
       if (this.total > this.count && this.busy == false) {
         this.busy = true;
 
-     
-          this.items2.splice(0);
-          for (var i = 0, j = 12; i < j; i++) {
-            let api = this.allitems[this.count];
+        this.items2.splice(0);
+        for (var i = 0, j = 12; i < j; i++) {
+          let api = this.allitems[this.count];
 
-            this.items2.push(api);
-            this.count++;
-          }
+          this.items2.push(api);
+          this.count++;
+        }
 
-          this.busy = false;
-        
+        this.busy = false;
       }
     },
 
@@ -77,7 +81,7 @@ export default {
 
         this.$store.commit("SET_NAVHEIGHT", height - 1);
       }
-    }
+    },
   },
   watch: {
     // whenever question changes, this function will run
@@ -89,20 +93,19 @@ export default {
       } else if (this.$route.query.page == null) {
         this.$route.query.page = 1;
         this.loadMore();
-          this.navHeight();
+        this.navHeight();
         this.pageCheck();
       } else {
         this.loadMore();
-          this.navHeight();
+        this.navHeight();
         this.pageCheck();
       }
     },
-    queryParam: function() {
+    queryParam: function () {
       this.loadMore();
-    }
+    },
   },
   computed: {
-
     offset() {
       if (this.queryParam > 1) {
         return Number(this.queryParam - 1) * 12;
@@ -131,6 +134,10 @@ export default {
       } else {
         return Number(this.$route.query.page);
       }
+    },
+
+    activeUsers() {
+      return items[pi];
     }
   },
 
@@ -155,7 +162,7 @@ export default {
   beforeDestroy() {
     // Unregister the event listener before destroying this Vue instance
     window.removeEventListener("resize", this.onResize);
-  }
+  },
 };
 </script>
 
--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,26 +1,37 @@
 <template>
   <main>
-    <div class="full-height single xs-border-left xs-border-right" :style="`min-height:calc(100vh - ${navbarheight}px);margin-top:${navbarheight}px`">
+    <div
+      class="full-height single xs-border-left xs-border-right"
+      :style="`min-height:calc(100vh - ${navbarheight}px);margin-top:${navbarheight}px`"
+    >
       <div class="xs-mt2 xs-p2 bcg-item">
         <div class="item xs-block xs-full-height">
-          <div class="feat-wrapper" v-if="theThumb"><transition appear name="fade"><img class="featured-image" :src="thumbnail" :alt="title"></transition></div>
+          <div class="feat-wrapper" v-if="theThumb">
+            <transition appear name="fade">
+              <img class="featured-image" :src="thumbnail" :alt="title" />
+            </transition>
+          </div>
           <h1 class="xs-py3 main-title">{{title}}</h1>
           <no-ssr>
             <div class="xs-mt-5 bold">
-            <ul class="list-unstyled xs-flex xs-flex-align-center">
-             <li class="xs-inline-block xs-mr1" v-if="this.$store.state.theCategory"><div class="tag fill-gray-darker xs-border">
-  <nuxt-link :to="`/category/${this.$store.state.theCategory.replace(/\s+/g, '-').toLowerCase()}`" class="tag__link text-white">{{this.$store.state.theCategory}}</nuxt-link>
-</div></li> 
-<!-- <li class="xs-inline-block">{{ date }}</li> -->
-              </ul> 
-             </div>
+              <ul class="list-unstyled xs-flex xs-flex-align-center">
+                <li class="xs-inline-block xs-mr1" v-if="this.$store.state.theCategory">
+                  <div class="tag fill-gray-darker xs-border">
+                    <nuxt-link
+                      :to="`/category/${this.$store.state.theCategory.replace(/\s+/g, '-').toLowerCase()}`"
+                      class="tag__link text-white"
+                    >{{this.$store.state.theCategory}}</nuxt-link>
+                  </div>
+                </li>
+                <!-- <li class="xs-inline-block">{{ date }}</li> -->
+              </ul>
+            </div>
           </no-ssr>
           <div class="xs-py3 post-content text-gray-lighter">
             <div v-html="$md.render(body)"></div>
           </div>
         </div>
       </div>
-
     </div>
   </main>
 </template>
@@ -33,23 +44,27 @@ import MdWrapper from "~/components/MdWrapper";
 export default {
   async asyncData({ params, app, payload, route, store }) {
     let post = await import("~/content/blog/posts/" + params.slug + ".json");
-   
-   await store.commit("SET_TITLE", post.title);
-     await store.commit("SET_THUMB", post.thumbnail);
-     await store.commit("SET_CRUMB", 'Categories');
-   await   store.commit("SET_POSTCAT", post.category);
+
+    await store.commit("SET_TITLE", post.title);
+    await store.commit("SET_THUMB", post.thumbnail);
+    await store.commit("SET_CRUMB", "Categories");
+    await store.commit("SET_POSTCAT", post.category);
     await store.commit("paginateOff", false);
     return post;
   },
-   transition (to, from) {
-    if (!from) { return 'slide-left' } else {return 'slide-right'}
+  transition(to, from) {
+    if (!from) {
+      return "slide-left";
+    } else {
+      return "slide-right";
+    }
   },
   head() {
     return {
-      title: this.title + " | " + this.$store.state.siteInfo.sitename
+      title: this.title + " | " + this.$store.state.siteInfo.sitename,
     };
   },
-  
+
   data() {
     return {};
   },
@@ -62,7 +77,7 @@ export default {
     navHeight() {
       var height = document.getElementById("navbar").clientHeight;
       this.$store.commit("SET_NAVHEIGHT", height);
-    }
+    },
   },
   updated() {
     if (process.browser) {
@@ -97,10 +112,10 @@ export default {
     },
     navbarheight() {
       return this.$store.state.navheight;
-    }
+    },
   },
   components: {
-    MdWrapper
-  }
+    MdWrapper,
+  },
 };
 </script>
